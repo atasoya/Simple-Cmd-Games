0b6b414d6482c4522ade32e7c9ea2baf44332d88
atasoya <ataatasoy2013@gmail.com>
2023-12-25 19:09:31 +0200

    add home page

--- main.go
+++ main.go
@@ -12,17 +12,38 @@ func main() {
 
 	args := os.Args[1:]
 
-	switch args[0] {
-	case "-h":
-		Help_command.Help()
-	case "-H":
-		Help_command.Help()
-	case "-hr":
-		duration, horse_count := Horse_command.Validate(args)
+	if len(args) == 0 {
+		asciiArt := `
+	    _____ _____ __  __ _____  _      ______                    
+	   / ____|_   _|  \/  |  __ \| |    |  ____|                   
+	  | (___   | | | \  / | |__) | |    | |__                      
+	   \___ \  | | | |\/| |  ___/| |    |  __|                     
+	   ____) |_| |_| |  | | |    | |____| |____                    
+	  |_____/|_____|_|__|_|_| ___|______|______| __ ______  _____  
+	   / ____| |    |_   _|  / ____|   /\   |  \/  |  ____|/ ____| 
+	  | |    | |      | |   | |  __   /  \  | \  / | |__  | (___   
+	  | |    | |      | |   | | |_ | / /\ \ | |\/| |  __|  \___ \  
+	  | |____| |____ _| |_  | |__| |/ ____ \| |  | | |____ ____) | 
+	   \_____|______|_____|  \_____/_/    \_\_|  |_|______|_____/  
+	  |_   _| \ | |  / ____|/ __ \                                 
+	    | | |  \| | | |  __| |  | |                                
+	    | | | .   | | | |_ | |  | |                                
+	   _| |_| |\  | | |__| | |__| |                                
+	  |_____|_| \_|  \_____|\____/                                 
+																   
+	  -h to see all commands															   
+	 `
+		fmt.Println(asciiArt)
+	} else {
+		switch args[0] {
+		case "-h":
+			Help_command.Help()
+		case "-H":
+			Help_command.Help()
+		case "-hr":
+			duration, horse_count := Horse_command.Validate(args)
 
-		Horse_command.Horse_race(horse_count, duration)
-	default:
-		fmt.Println(args)
+			Horse_command.Horse_race(horse_count, duration)
+		}
 	}
-
 }
